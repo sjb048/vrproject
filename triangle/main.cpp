71dd32466634803579cad2bfaf9425c876155888
sjb048 <brinti048@gmail.com>
2024-10-16 13:45:03 +0200

    Initial Commit

---
+++ triangle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  triangle
+//
+//  Created by Sumaia Jahan Brinti on 16/10/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
